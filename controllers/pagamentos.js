39d94a034c867845034bba8e4e14426e0678591c
Yago Souza Santos <yago.souza.santos@gmail.com>
2017-02-18 16:27:33 -0200

    Alterando para pagamentos.

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -1,6 +1,11 @@
-module.exports = function(app){
-  app.get('/pagamentos', function(req, res){
+module.exports = function (app) {
+  app.get('/pedidos/lista', function (req, res) {
     console.log('Recebida requisicao de teste na porta 3000.')
     res.send('OK.');
   });
+  app.post('/pedidos/adiciona', function (req, res) {
+    var pagamento = req.body;
+    res.send('OK.')
+  });
+
 }
