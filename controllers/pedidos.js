5361ed7ed1a9f6ad17fde17d57312e22210ba05b
Ádler Neves <adlerosn@gmail.com>
2017-04-25 02:15:11 -0300

    little update

--- controllers/pedidos.js
+++ controllers/pedidos.js
@@ -1,133 +1,137 @@
 module.exports = function (app) {
-  var fs = require('fs');
+    var fs = require('fs');
 
-  app.get('/pedidos/lista', function (req, res) {
-
-    var path = require('path');
-
-    var filePath = path.join(__dirname, 'pedidos.txt');
-
-    fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
-      if (!err) {
-        res.json(data);
-      } else {
-        console.log(err);
-      }
+    app.get('/', function (req, res) {
+        res.text('API root; nothing here.');
     });
-  });
-  app.post('/pedidos/adiciona', function (req, res) {
 
-    var path = require('path');
+    app.get('/pedidos/lista', function (req, res) {
 
-    var filePath = path.join(__dirname, 'pedidos.txt');
+        var path = require('path');
 
-    var pedido = req.body;
-    console.log(pedido);
-    var json = JSON.stringify(pedido);
-    var obj = [];
-    console.log(json);
-    fs.readFile(filePath, 'utf8', function readFileCallback(err, data) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("data " + data.length);
-        if (data.length == 2) {
-          fs.readFile(filePath, 'utf8', function readFileCallback(err, data) {
-            if (err) {
-              console.log(err);
+        var filePath = path.join(__dirname, 'pedidos.txt');
+
+        fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
+            if (!err) {
+                res.json(data);
             } else {
-              var ress = data.replace(']', json + ']');
-              console.log('nova data :' + ress);
-              fs.writeFile(filePath, ress, 'utf8', function (err) { }); // write it back 
+                console.log(err);
             }
-          });
-        } else {
-          fs.readFile(filePath, 'utf8', function readFileCallback(err, data) {
+        });
+    });
+    app.post('/pedidos/adiciona', function (req, res) {
+
+        var path = require('path');
+
+        var filePath = path.join(__dirname, 'pedidos.txt');
+
+        var pedido = req.body;
+        console.log(pedido);
+        var json = JSON.stringify(pedido);
+        var obj = [];
+        console.log(json);
+        fs.readFile(filePath, 'utf8', function readFileCallback(err, data) {
             if (err) {
-              console.log(err);
+                console.log(err);
             } else {
+                console.log("data " + data.length);
+                if (data.length == 2) {
+                    fs.readFile(filePath, 'utf8', function readFileCallback(err, data) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            var ress = data.replace(']', json + ']');
+                            console.log('nova data :' + ress);
+                            fs.writeFile(filePath, ress, 'utf8', function (err) { }); // write it back
+                        }
+                    });
+                } else {
+                    fs.readFile(filePath, 'utf8', function readFileCallback(err, data) {
+                        if (err) {
+                            console.log(err);
+                        } else {
 
-              var lasti = data.lastIndexOf(']');
-              var ress = data.substring(0, lasti) + ',' + json + ']';
+                            var lasti = data.lastIndexOf(']');
+                            var ress = data.substring(0, lasti) + ',' + json + ']';
 
-              //  = data.replace(']', ',' + json + ']');
-              console.log('nova data :' + ress);
+                            //  = data.replace(']', ',' + json + ']');
+                            console.log('nova data :' + ress);
 
-              fs.writeFile(filePath, ress, 'utf8', function (err) { }); // write it back 
+                            fs.writeFile(filePath, ress, 'utf8', function (err) { }); // write it back
+                        }
+                    });
+                }
             }
-          });
-        }
-      }
-    });
-  });
-
-
-
-
-  app.get('/pedidos/id', function (req, res) {
-
-    var path = require('path');
-
-    var filePath = path.join(__dirname, 'pedidos.txt');
-
-    var pedido = req.body;
-    console.log(pedido);
-    var json = JSON.stringify(pedido);
-    var obj = [];
-    console.log(json);
-    fs.readFile(filePath, 'utf8', function readFileCallback(err, data) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("data " + data.length);
-
-        fs.readFile(filePath, 'utf8', function readFileCallback(err, data) {
-          if (err) {
-            console.log(err);
-          } else {
-            var ress = JSON.parse(data);
-            res.json(Object.keys(data).length);
-          }
         });
-
-      }
     });
-  });
 
-  app.get('/pedidos/lista/zera', function (req, res) {
 
-    var path = require('path');
 
-    var filePath = path.join(__dirname, 'pedidos.txt');
 
-    var pedido = req.body;
-    console.log(pedido);
-    var json = JSON.stringify(pedido);
-    var obj = [];
-    console.log(json);
-    fs.readFile(filePath, 'utf8', function readFileCallback(err, data) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("data " + data.length);
+    app.get('/pedidos/id', function (req, res) {
+
+        var path = require('path');
+
+        var filePath = path.join(__dirname, 'pedidos.txt');
+
+        var pedido = req.body;
+        console.log(pedido);
+        var json = JSON.stringify(pedido);
+        var obj = [];
+        console.log(json);
         fs.readFile(filePath, 'utf8', function readFileCallback(err, data) {
-          if (err) {
-            console.log(err);
-          } else {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("data " + data.length);
 
-            var lasti = data.lastIndexOf(']');
-            var ress = data.substring(0, lasti) + ',' + json + ']';
+                fs.readFile(filePath, 'utf8', function readFileCallback(err, data) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        var ress = JSON.parse(data);
+                        res.json(Object.keys(data).length);
+                    }
+                });
 
-            //  = data.replace(']', ',' + json + ']');
-            console.log('nova data :' + ress);
-
-            fs.writeFile(filePath, `[]`, 'utf8', function (err) { }); // write it back 
-            res.json('');
-          }
+            }
         });
-
-      }
     });
-  });
+
+    app.get('/pedidos/lista/zera', function (req, res) {
+
+        var path = require('path');
+
+        var filePath = path.join(__dirname, 'pedidos.txt');
+
+        var pedido = req.body;
+        console.log(pedido);
+        var json = JSON.stringify(pedido);
+        var obj = [];
+        console.log(json);
+        fs.readFile(filePath, 'utf8', function readFileCallback(err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("data " + data.length);
+                fs.readFile(filePath, 'utf8', function readFileCallback(err, data) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+
+                        var lasti = data.lastIndexOf(']');
+                        var ress = data.substring(0, lasti) + ',' + json + ']';
+
+                        //  = data.replace(']', ',' + json + ']');
+                        console.log('nova data :' + ress);
+
+                        fs.writeFile(filePath, `[]`, 'utf8', function (err) { }); // write it back
+                        res.json('');
+                    }
+                });
+
+            }
+        });
+    });
 
 }
