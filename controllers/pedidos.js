9bab118c3797e817790ed0536f713270c252da35
Ádler Neves <adlerosn@gmail.com>
2017-04-25 03:43:34 -0300

    little update

--- controllers/pedidos.js
+++ controllers/pedidos.js
@@ -3,6 +3,32 @@ var rootFunc = function (req, res) {
         type:'index',
         message:'API root; nothing here.',
         data:{
+            'tela/{nome}':[
+                {
+                    method:'GET',
+                    description:'Retorna uma tela: "cotar", "ofertar" ou "estoque"'
+                }
+            ],
+            'estoque':[
+                {
+                    method:'GET',
+                    description:'Retorna a lista de produtos em estoque'
+                },
+                {
+                    method:'POST',
+                    description:'Insere um produto no estoque'
+                }
+            ],
+            'estoque/{produto_id}':[
+                {
+                    method:'GET',
+                    description:'Retorna um produto do estoque'
+                },
+                {
+                    method:'POST',
+                    description:'Altera um produto do estoque'
+                }
+            ],
             'pedido':[
                 {
                     method:'GET',
@@ -54,6 +80,48 @@ module.exports = function (app) {
     app.patch('/', rootFunc);
     app.delete('/', rootFunc);
 
+    app.get('/tela', function (req, res) {
+        res.send(
+            '<ul>'+
+            '<li>'+'<a href="tela/cotar">Cotar</a>'+'</li>'+
+            '<li>'+'<a href="tela/ofertar">Ofertar</a>'+'</li>'+
+            '<li>'+'<a href="tela/estoque">Estoque</a>'+'</li>'+
+            '</ul>'
+        );
+    });
+    app.get('/tela/:nome', function (req, res) {
+        switch(req.params.nome){
+            case 'cotar':
+            fs.readFile('./cotar.html', function(err, data){
+                res.send(data);
+            });
+            break;
+            case 'ofertar':
+            fs.readFile('./ofertar.html', function(err, data){
+                res.send(data);
+            });
+            break;
+            case 'estoque':
+            fs.readFile('./estoque.html', function(err, data){
+                res.send(data);
+            });
+            break;
+            default:
+            res.send('');
+        }
+    });
+    app.get('/estoque', function (req, res) {
+        res.json(null);
+    });
+    app.post('/estoque', function (req, res) {
+        res.json(null);
+    });
+    app.get('/estoque/:produto_id', function (req, res) {
+        res.json(null);
+    });
+    app.post('/estoque/:produto_id', function (req, res) {
+        res.json(null);
+    });
     app.get('/pedido', function (req, res) {
         res.json(null);
     });
