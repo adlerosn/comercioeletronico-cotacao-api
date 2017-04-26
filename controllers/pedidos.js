d0d395cabdbd9b615d92985803474b0ea7ff6525
Ádler Neves <adlerosn@gmail.com>
2017-04-26 02:53:22 -0300

    post data filters

--- controllers/pedidos.js
+++ controllers/pedidos.js
@@ -213,14 +213,219 @@ function getPedidoList(){
     return ensureArray(loadDb().pedido);
 }
 
-function filterProduto(res){
-    var prod = {};
-    return prod;
+/*
+* item : object{
+*   codigo    : string : codigo do produto
+*   descricao : string : descricao do produto
+*   nome      : string : nome curto
+*   qtdunt    : string : quantidade unitária [ex: caixa de 5 kg]
+*   qtdest    : float  : quantidade em estoque
+* }
+*/
+function filterProduto(req){
+    var prod_i = req;
+    var prod_o = {};
+    prod_o.codigo = String(ensureNotUndefined(prod_i.codigo,""));
+    prod_o.descricao = String(ensureNotUndefined(prod_i.descricao,""));
+    prod_o.nome = String(ensureNotUndefined(prod_i.nome,""));
+    prod_o.qtdunt = String(ensureNotUndefined(prod_i.qtdunt,""));
+    prod_o.qtdest = parseFloat(ensureNotUndefined(prod_i.qtdest,NaN));
+    if(isNaN(prod_o.qtdest)){
+        prod_o.qtdest = 0.0;
+    }
+    if(
+        prod_o.codigo.length==0
+        ||
+        prod_o.descricao.length==0
+        ||
+        prod_o.nome.length==0
+        ||
+        prod_o.qtdunt.length==0
+        ||
+        prod_o.qtdest <= 0
+    ){
+        return null;
+    }
+    return prod_o;
 }
 
-function filterPedido(res){
-    var ped = {};
-    return ped;
+/*
+* item : object{
+*   codigo    : string : codigo do produto
+*   descricao : string : descricao do produto
+*   nome      : string : nome curto
+*   qtdunt    : string : quantidade unitária [ex: caixa de 5 kg]
+*   qtdund    : float  : quantidade de unidades
+* }
+*/
+function filterPedidoItem(req){
+    var itm_i = req;
+    var itm_o = {};
+    itm_o.codigo = String(ensureNotUndefined(itm_i.codigo,""));
+    itm_o.descricao = String(ensureNotUndefined(itm_i.descricao,""));
+    itm_o.nome = String(ensureNotUndefined(itm_i.nome,""));
+    itm_o.qtdunt = String(ensureNotUndefined(itm_i.qtdunt,""));
+    itm_o.qtdund = parseFloat(ensureNotUndefined(itm_i.qtdund,NaN));
+    if(isNaN(itm_o.qtdund)){
+        itm_o.qtdund = 0.0;
+    }
+    if(
+        itm_o.codigo.length==0
+        ||
+        itm_o.descricao.length==0
+        ||
+        itm_o.nome.length==0
+        ||
+        itm_o.qtdunt.length==0
+        ||
+        itm_o.qtdund <= 0
+    ){
+        return null;
+    }
+    return itm_o;
+}
+
+/*
+* pedido : object{
+*   cliente : string : nome do cliente
+*   data    : int    : timestamp de inserção
+*   itens   : item[] : item do pedido
+* }
+*/
+function filterPedido(req){
+    var ped_i = req;
+    var ped_o = {};
+    ped_o.cliente = String(ensureNotUndefined(ped_i.cliente,""));
+    ped_o.data = parseInt(ensureNotUndefined(ped_i.data,NaN));
+    if(isNaN(ped_o.data)){
+        ped_o.data = parseInt((new Date()).getTime()/1000);
+    }
+    ped_o.itens = new Array();
+    for(var item of ensureArray(ped_i.itens)){
+        var itemFiltered = filterPedidoItem(item);
+        if(itemFiltered){
+            ped_o.itens.push(itemFiltered)
+        }
+    }
+    if(ped_o.cliente.length==0 || ped_o.itens.length==0){
+        return null;
+    }
+    return ped_o;
+}
+
+/*
+* item : object{
+*   codigo    : string : codigo do produto
+*   descricao : string : descricao
+*   marca     : string : marca/modelo
+*   nome      : string : nome curto
+*   qtdunt    : string : quantidade unitária [ex: caixa de 5 kg]
+*   qtdund    : float  : quantidade de unidades
+*   valunt    : float  : valor unitário
+*   valtt     : float  : valor total do item
+* }
+*/
+function filterCotacaoItem(req){
+    var itm_i = req;
+    var itm_o = {};
+    itm_o.codigo = String(ensureNotUndefined(itm_i.codigo,""));
+    itm_o.descricao = String(ensureNotUndefined(itm_i.descricao,""));
+    itm_o.nome = String(ensureNotUndefined(itm_i.nome,""));
+    itm_o.qtdunt = String(ensureNotUndefined(itm_i.qtdunt,""));
+    itm_o.qtdund = parseFloat(ensureNotUndefined(itm_i.qtdund,NaN));
+    itm_o.valunt = parseFloat(ensureNotUndefined(itm_i.valunt,NaN));
+    itm_o.valtt = parseFloat(ensureNotUndefined(itm_i.valtt,NaN));
+    if(isNaN(itm_o.qtdund)){
+        itm_o.qtdund = 0.0;
+    }
+    if(isNaN(itm_o.valunt)){
+        itm_o.valunt = 0.0;
+    }
+    if(isNaN(itm_o.valtt)){
+        itm_o.valtt = 0.0;
+    }
+    if(
+        itm_o.codigo.length==0
+        ||
+        itm_o.descricao.length==0
+        ||
+        itm_o.nome.length==0
+        ||
+        itm_o.qtdunt.length==0
+        ||
+        itm_o.qtdund <= 0
+        ||
+        itm_o.valunt <= 0
+        ||
+        itm_o.valtt <= 0
+    ){
+        return null;
+    }
+    return itm_o;
+}
+
+/*
+* cotacao : object{
+*   nome     : string    : Nome do fornecedor
+*   vendedor : string    : Vendedor
+*   telefone : string    : Telefone de contado do vendedor
+*   email    : string    : Email do vendedor
+*   data     : int       : Data da cotação
+*   valtt    : float     : Valor total do pedido
+*   fpag1    : string    : Forma de pagamento 1
+*   fpag2    : string    : Forma de pagamento 2
+*   validade : int       : Validade da proposta
+*   itensped : itemped[] : Itens do pedido
+* }
+*/
+function filterCotacao(req){
+    var cot_i = req;
+    var cot_o = {};
+    cot_o.nome = String(ensureNotUndefined(cot_i.nome,""));
+    cot_o.vendedor = String(ensureNotUndefined(cot_i.vendedor,""));
+    cot_o.telefone = String(ensureNotUndefined(cot_i.telefone,""));
+    cot_o.email = String(ensureNotUndefined(cot_i.email,""));
+    cot_o.fpag1 = String(ensureNotUndefined(cot_i.fpag1,""));
+    cot_o.fpag2 = String(ensureNotUndefined(cot_i.fpag2,""));
+    cot_o.data = parseInt(ensureNotUndefined(cot_i.data,NaN));
+    if(isNaN(cot_o.data)){
+        cot_o.data = parseInt((new Date()).getTime()/1000);
+    }
+    cot_o.valtt = parseFloat(ensureNotUndefined(cot_i.valtt,NaN));
+    if(isNaN(cot_o.valtt)){
+        cot_o.valtt = 0.0;
+    }
+    cot_o.validade = parseInt(ensureNotUndefined(cot_i.validade,NaN));
+    if(isNaN(cot_o.validade)){
+        cot_o.validade = parseInt((new Date()).getTime()/1000);
+    }
+    cot_o.itensped = new Array();
+    for(var item of ensureArray(cot_i.itensped)){
+        var itemFiltered = filterCotacaoItem(item);
+        if(itemFiltered){
+            cot_o.itens.push(itemFiltered)
+        }
+    }
+    if(
+        cot_o.nome.length==0
+        ||
+        cot_o.vendedor.length==0
+        ||
+        cot_o.telefone.length==0
+        ||
+        cot_o.email.length==0
+        ||
+        cot_o.fpag1.length==0
+        ||
+        cot_o.fpag2.length==0
+        ||
+        cot_o.itensped.length==0
+        ||
+        cot_o.data > cot_o.validade
+    ){
+        return null;
+    }
+    return cot_o;
 }
 
 module.exports = function (app) {
@@ -245,16 +450,19 @@ module.exports = function (app) {
         switch(req.params.nome){
             case 'cotar':
             fs.readFile('./cotar.html', function(err, data){
+                res.set('content-type','text/html');
                 res.send(data);
             });
             break;
             case 'ofertar':
             fs.readFile('./ofertar.html', function(err, data){
+                res.set('content-type','text/html');
                 res.send(data);
             });
             break;
             case 'estoque':
             fs.readFile('./estoque.html', function(err, data){
+                res.set('content-type','text/html');
                 res.send(data);
             });
             break;
@@ -266,7 +474,7 @@ module.exports = function (app) {
         res.json(getEstoqueList());
     });
     app.post('/estoque', function (req, res) {
-        var novo = filterProduto(res);
+        var novo = filterProduto(permissiveJsonParse(req.body,{}));
         if(novo){
             var list = getEstoqueList();
             var produto_id = indexArrayAutoincrement(list);
@@ -285,7 +493,7 @@ module.exports = function (app) {
     app.post('/estoque/:produto_id', function (req, res) {
         var produto_id = parseInt(req.params.produto_id);
         if(!isNaN(produto_id) && produto_id>=0){
-            var novo = filterProduto(res);
+            var novo = filterProduto(permissiveJsonParse(req.body,{}));
             var list = getEstoqueList();
             novo.id = produto_id;
             list[produto_id] = novo;
@@ -296,11 +504,22 @@ module.exports = function (app) {
             res.json(false);
         }
     });
+    app.post('/busca', function (req, res) {
+        var termo = ensureNotUndefined(permissiveJsonParse(req.body,""),"");
+        var resultado = new Array();
+        var list = getEstoqueList();
+        for(var item of list){
+            if(item.indexOf(termo) !== -1){
+                resultado.push(item);
+            }
+        }
+        res.json(resultado);
+    });
     app.get('/pedido', function (req, res) {
         res.json(getPedidoList());
     });
     app.post('/pedido', function (req, res) {
-        var novo = filterPedido(res);
+        var novo = filterPedido(permissiveJsonParse(req.body,{}));
         if(novo){
             var list = getPedidoList();
             var pedido_id = indexArrayAutoincrement(list);
@@ -317,7 +536,7 @@ module.exports = function (app) {
         res.json(getArrayItem(getPedidoList(), req.params.pedido_id, null));
     });
     app.get('/pedido/:pedido_id/cotacao', function (req, res) {
-        pedido = getArrayItem(getPedidoList(), req.params.pedido_id, null);
+        var pedido = getArrayItem(getPedidoList(), req.params.pedido_id, null);
         if(pedido){
             res.json(ensureArray(pedido.cotacao));
         }else{
@@ -325,38 +544,41 @@ module.exports = function (app) {
         }
     });
     app.post('/pedido/:pedido_id/cotacao', function (req, res) {
-        pedido = getArrayItem(getPedidoList(), req.params.pedido_id, null);
+        var novo = filterCotacao(permissiveJsonParse(req.body,{}));
+        var pedido = getArrayItem(getPedidoList(), req.params.pedido_id, null);
         if(pedido){
-            res.json(ensureArray(pedido.cotacao));
+            var cotacoes = ensureArray(pedido.cotacao);
+            var cotacao_id = indexArrayAutoincrement(cotacoes);
+            novo.id = cotacao_id;
+            cotacoes[cotacao_id] = novo;
+            _db.pedido[pedido.id].cotacao = cotacoes;
+            saveDb(_db);
+            res.json(cotacao_id);
         }else{
             res.json(false);
         }
     });
     app.get('/pedido/:pedido_id/cotacao/:cotacao_id', function (req, res) {
-        pedido = getArrayItem(getPedidoList(), req.params.pedido_id, null);
+        var pedido = getArrayItem(getPedidoList(), req.params.pedido_id, null);
         if(pedido){
-            cotacoes = ensureArray(pedido.cotacao);
-            cotacao = getArrayItem(cotacoes, req.params.cotacao_id, null);
+            var cotacoes = ensureArray(pedido.cotacao);
+            var cotacao = getArrayItem(cotacoes, req.params.cotacao_id, null);
             res.json(cotacao);
         }else{
             res.json(null);
         }
     });
     app.get('/pedido/:pedido_id/cotacao/:cotacao_id/islatest', function (req, res) {
-        pedido = getArrayItem(getPedidoList(), req.params.pedido_id, null);
+        var pedido = getArrayItem(getPedidoList(), req.params.pedido_id, null);
         if(pedido){
-            cotacoes = ensureArray(pedido.cotacao);
+            var cotacoes = ensureArray(pedido.cotacao);
             var cotacaoMaisRecente = -1;
             for(cotacao of cotacoes){
                 if(cotacao && cotacao.id > cotacaoMaisRecente){
                     cotacaoMaisRecente = cotacao.id;
                 }
             }
-            if(cotacaoMaisRecente>-1 && cotacaoMaisRecente===parseInt(req.params.cotacao_id)){
-                res.json(true);
-            }else{
-                res.json(false);
-            }
+            res.json(cotacaoMaisRecente>-1 && cotacaoMaisRecente===parseInt(req.params.cotacao_id));
         }else{
             res.json(false);
         }
